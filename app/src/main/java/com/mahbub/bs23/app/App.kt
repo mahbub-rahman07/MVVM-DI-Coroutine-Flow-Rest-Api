58910a7794f7c53a2b4382fdb1eb7b794cc6a92f
Md. Mahbubur Rahman <mahbubmridha07@gmail.com>
2022-07-20 11:24:20 +0600

    added debug suffix and timber for debug mode

--- app/src/main/java/com/mahbub/bs23/app/App.kt
+++ app/src/main/java/com/mahbub/bs23/app/App.kt
@@ -1,12 +1,17 @@
 package com.mahbub.bs23.app
 
 import android.app.Application
+import com.mahbub.bs23.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         ObjectBox.init(this)
+        if(BuildConfig.DEBUG){
+            Timber.plant( Timber.DebugTree())
+        }
     }
 }
